6db059ae8d67f6bd52341d94f58f3bbacdbf1b79
elias-a <elias.alstead@gmail.com>
2022-01-30 22:54:51 -0500

    Scrape hello fresh meals for upcoming week

--- getData.py
+++ getData.py
@@ -16,10 +16,28 @@ options.headless = True
 options.add_experimental_option("debuggerAddress", f"127.0.0.1:{port}")
 driver = webdriver.Chrome(chromedriver, options=options)
 
-mealTitles = driver.find_elements(By.XPATH, "//h4[@data-test-id='recipe-card-title']")
-meals = [title.get_attribute('title') for title in mealTitles]
+# Get all meals that have been previously ordered. 
+mealTitleXPath = "//h4[@data-test-id='recipe-card-title']"
+pastMealTitles = driver.find_elements(By.XPATH, mealTitleXPath)
+pastMeals = [title.get_attribute('title') for title in pastMealTitles]
 
-with open("meals.pickle", "wb") as f:
-    dump(meals, f)
+# TODO: Add logic to navigate to second page. 
+driver.get("")
+
+# Get all meals for the upcoming week, both selected and unselected. 
+# Exclude the premium meals. 
+recipeXPath = "div[@data-test-wrapper-id='recipe-component']"
+selectedMealsXPath = "span[@data-translation-id='my-deliveries-experiments.multiple-up.in-your-box']"
+unselectedMealsTitles = driver.find_elements(By.XPATH, f"{mealTitleXPath}[ancestor::{recipeXPath}[not(@data-test-id='recipe-is-premium')][not(descendant::{selectedMealsXPath})]]")
+selectedMealsTitles = driver.find_elements(By.XPATH, f"{mealTitleXPath}[ancestor::{recipeXPath}[descendant::{selectedMealsXPath}]]")
+unselectedMeals = [title.get_attribute('title') for title in unselectedMealsTitles]
+selectedMeals = [title.get_attribute('title') for title in selectedMealsTitles]
+
+selectedMeals += pastMeals
+
+with open("selected-meals.pickle", "wb") as f:
+    dump(selectedMeals, f)
+with open("unselected-meals.pickle", "wb") as f:
+    dump(unselectedMeals, f)
 
 driver.quit()
