4db22cb9365fe30ba997fd72ce941ebd5e7a1e02
elias-a <elias.alstead@gmail.com>
2022-02-19 17:35:19 -0500

    Package the meal selection functionality in a class

--- analyzeData.py
+++ analyzeData.py
@@ -1,68 +1,84 @@
 import re
+import nltk
 import pandas as pd
 from pickle import load
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
+class Analyze:
 
-with open("selected-meals.pickle", "rb") as f:
-    selectedMeals = load(f)
-with open("unselected-meals.pickle", "rb") as f:
-    unselectedMeals = load(f)
+    def __init__(self):
+        self.stopwords = []
+        self.selectedCounts = {}
+        self.selectedLength = 0
+        self.unselectedCounts = {}
+        self.unselectedLength = 0
+        self.scores = []
 
-englishStopwords = stopwords.words('english')
-frenchStopwords = stopwords.words('french')
-allStopwords = englishStopwords + frenchStopwords
+    def preprocess(self, meal):
+        # Convert to lowercase
+        meal = meal.lower()
+        # Remove punctuation
+        meal = re.sub(r'[^\w\s]', '', meal)
+        # Tokenize
+        meal = word_tokenize(meal)
+        # Remove stopwords
+        meal = [token for token in meal if token not in self.stopwords]
 
-def preprocess(meal):
-    # Convert to lowercase
-    meal = meal.lower()
-    # Remove punctuation
-    meal = re.sub(r'[^\w\s]', '', meal)
-    # Tokenize
-    meal = word_tokenize(meal)
-    # Remove stopwords
-    meal = [token for token in meal if token not in allStopwords]
+        return meal
 
-    return meal
+    # Get the frequency of the appearance of word as a 
+    # percentage of the total number of words. 
+    @staticmethod
+    def getWordFreqPercentage(word, vocabCounts, vocabLength):
+        try:
+            return vocabCounts[word] / vocabLength
+        except KeyError:
+            return 0
 
-selectedMeals = [preprocess(meal) for meal in selectedMeals]
-unselectedMeals = [preprocess(meal) for meal in unselectedMeals]
+    def computeScore(self, meal):
+        selectedScore = sum([
+            Analyze.getWordFreqPercentage(word, self.selectedCounts, self.selectedLength) 
+            for word in meal])
+        unselectedScore = sum([
+            Analyze.getWordFreqPercentage(word, self.unselectedCounts, self.unselectedLength) 
+            for word in meal])
+        return selectedScore - unselectedScore
 
-# Convert meals to a list of words. 
-selected = pd.Series([word for meal in selectedMeals for word in meal])
-unselected = pd.Series([word for meal in unselectedMeals for word in meal])
+    def selectMeals(self):
+        nltk.download('stopwords')
+        nltk.download('punkt')
 
-# Count the appearances of each word. 
-selectedCounts = selected.value_counts()
-unselectedCounts = unselected.value_counts()
-selectedLength = len(selected)
-unselectedLength = len(unselected)
+        with open("selected-meals.pickle", "rb") as f:
+            selectedMeals = load(f)
+        with open("unselected-meals.pickle", "rb") as f:
+            unselectedMeals = load(f)
 
-# Get the frequency of the appearance of word as a 
-# percentage of the total number of words. 
-def getWordFreqPercentage(word, vocabCounts, vocabLength):
-    try:
-        return vocabCounts[word] / vocabLength
-    except KeyError:
-        return 0
+        englishStopwords = stopwords.words('english')
+        frenchStopwords = stopwords.words('french')
+        allStopwords = englishStopwords + frenchStopwords
 
-# Use the word frequency percentage to rank the meals for 
-# the upcoming week. 
-with open("upcoming-meals.pickle", "rb") as f:
-    upcomingMeals = load(f)
+        selectedMeals = [self.preprocess(meal) for meal in selectedMeals]
+        unselectedMeals = [self.preprocess(meal) for meal in unselectedMeals]
 
-# Preprocess upcoming meals. 
-upcomingMeals = [(meal, preprocess(meal)) for meal in upcomingMeals]
+        # Convert meals to a list of words. 
+        selected = pd.Series([word for meal in selectedMeals for word in meal])
+        unselected = pd.Series([word for meal in unselectedMeals for word in meal])
 
-def computeScore(meal):
-    selectedScore = sum([getWordFreqPercentage(word, selectedCounts, selectedLength) for word in meal])
-    unselectedScore = sum([getWordFreqPercentage(word, unselectedCounts, unselectedLength) for word in meal])
-    return selectedScore - unselectedScore
+        # Count the appearances of each word. 
+        self.selectedCounts = selected.value_counts()
+        self.unselectedCounts = unselected.value_counts()
+        self.selectedLength = len(selected)
+        self.unselectedLength = len(unselected)
 
-scores = [(mealName, computeScore(processedMealName)) for mealName, processedMealName in upcomingMeals]
-scores.sort(key=lambda score: score[1], reverse=True)
-print(scores)
+        # Use the word frequency percentage to rank the meals for 
+        # the upcoming week. 
+        with open("upcoming-meals.pickle", "rb") as f:
+            upcomingMeals = load(f)
+
+        # Preprocess upcoming meals. 
+        upcomingMeals = [(meal, self.preprocess(meal)) for meal in upcomingMeals]
+
+        scores = [(mealName, self.computeScore(processedMealName)) for mealName, processedMealName in upcomingMeals]
+        scores.sort(key=lambda score: score[1], reverse=True)
+        self.scores = scores
--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from pickle import dump
 from datetime import date
 from helpers import initDriver
 from getData import getPastMeals, getUpcomingMeals
+from analyzeData import Analyze
 
 def switch(arg):
-    args = ["h", "u"]
+    args = ["h", "u", "s"]
     if arg not in args:
         return
 
-    driver = initDriver()
-
     if arg == "h":
+        driver = initDriver()
+
         # 
         pastMeals = getPastMeals(driver)
         today = date.today()
@@ -23,7 +24,11 @@ def switch(arg):
         with open("unselected-meals.pickle", "wb") as f:
             dump(unselectedMeals, f)
 
+        driver.quit()
+
     elif arg == "u":
+        driver = initDriver()
+
         selectionDate = date(2021, 2, 27)
         selectedMeals, unselectedMeals = getUpcomingMeals(driver, selectionDate)
         meals = selectedMeals + unselectedMeals
@@ -31,12 +36,19 @@ def switch(arg):
         with open("upcoming-meals.pickle", "wb") as f:
             dump(meals, f)
 
-    driver.quit()
+        driver.quit()
+
+    elif arg == "s":
+        analyzer = Analyze()
+        analyzer.selectMeals()
+        print(analyzer.scores)
 
 def usage():
-    print("Usage: python main.py {h|u}\n"
+    print("Usage: python main.py {h|u|s}\n"
         + f"{' ' * 4}h: Get meal history\n"
-        + f"{' ' * 4}u: Get meals for an upcoming week")
+        + f"{' ' * 4}u: Get meals for an upcoming week\n"
+        + f"{' ' * 4}s: Select meals for an upcoming week"
+    )
 
 try:
     switch(sys.argv[1])
