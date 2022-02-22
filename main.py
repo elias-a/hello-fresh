18a2c0eadec96fc4416486281fd6e5063a0eb4e5
elias-a <elias.alstead@gmail.com>
2022-02-21 20:35:40 -0500

    Improve control flow of main script

--- main.py
+++ main.py
@@ -5,50 +5,6 @@ from ChromeDriver import ChromeDriver
 from getData import getPastMeals, getUpcomingMeals
 from analyzeData import Analyze
 
-def switch(arg):
-    args = ["h", "u", "s"]
-    if arg not in args:
-        return
-
-    if arg == "h":
-        try:
-            driver = ChromeDriver("config.ini")
-
-            # 
-            pastMeals = getPastMeals(driver)
-            today = date.today()
-            selectedMeals, unselectedMeals = getUpcomingMeals(driver, today)
-            selectedMeals += pastMeals
-
-            with open("selected-meals.pickle", "wb") as f:
-                dump(selectedMeals, f)
-            with open("unselected-meals.pickle", "wb") as f:
-                dump(unselectedMeals, f)
-        finally:
-            driver.closeChrome()
-
-    elif arg == "u":
-        try:
-            driver = ChromeDriver("config.ini")
-
-            selectionDate = date(2021, 2, 27)
-            selectedMeals, unselectedMeals = getUpcomingMeals(driver, selectionDate)
-            meals = selectedMeals + unselectedMeals
-
-            with open("upcoming-meals.pickle", "wb") as f:
-                dump(meals, f)
-        finally:
-            driver.closeChrome()
-
-    elif arg == "p":
-        analyzer = Analyze()
-        analyzer.selectMeals()
-        print(analyzer.scores)
-
-    elif arg == "s":
-        analyzer = Analyze()
-        analyzer.selectMeals()
-
 def usage():
     print("Usage: python main.py {h|u|p|s}\n"
         + f"{' ' * 4}h: Get meal history\n"
@@ -58,6 +14,49 @@ def usage():
     )
 
 try:
-    switch(sys.argv[1])
+    arg = sys.argv[1]
 except IndexError:
+    usage()
+    sys.exit(1)
+
+if arg == "h":
+    try:
+        driver = ChromeDriver("config.ini")
+
+        # 
+        pastMeals = getPastMeals(driver)
+        today = date.today()
+        selectedMeals, unselectedMeals = getUpcomingMeals(driver, today)
+        selectedMeals += pastMeals
+
+        with open("selected-meals.pickle", "wb") as f:
+            dump(selectedMeals, f)
+        with open("unselected-meals.pickle", "wb") as f:
+            dump(unselectedMeals, f)
+    finally:
+        driver.closeChrome()
+
+elif arg == "u":
+    try:
+        driver = ChromeDriver("config.ini")
+
+        selectionDate = date(2021, 2, 27)
+        selectedMeals, unselectedMeals = getUpcomingMeals(driver, selectionDate)
+        meals = selectedMeals + unselectedMeals
+
+        with open("upcoming-meals.pickle", "wb") as f:
+            dump(meals, f)
+    finally:
+        driver.closeChrome()
+
+elif arg == "p":
+    analyzer = Analyze()
+    analyzer.selectMeals()
+    print(analyzer.scores)
+
+elif arg == "s":
+    analyzer = Analyze()
+    analyzer.selectMeals()
+
+else:
     usage()
